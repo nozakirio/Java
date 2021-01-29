46dfe8f854c9384ea211d4eb38ddb1f70f120ddc
野崎梨緒 <wada.rio1001@gmail.com>
2021-01-29 22:58:35 +0900

    Finish lesson_4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "nozaki";
+		System.out.println(str1);
+		
+		str1 = "wada";
+		System.out.println(str1);
+		
+		final int LUCKY_NUM = 6;
+		System.out.println(LUCKY_NUM);
+	}
+}
