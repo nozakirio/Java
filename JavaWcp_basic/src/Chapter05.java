4af181f3992f780aed9673934954569b01ae80f4
野崎梨緒 <wada.rio1001@gmail.com>
2021-01-30 19:37:55 +0900

    Finish lesson_5

---
+++ JavaWcp_basic/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long a = 10000000000L;
+        System.out.println(a);
+        double b = 1.11;
+        System.out.println(b);
+        boolean c = true;
+        System.out.println(c);
+        
+        Integer d = 100;
+        System.out.println(d);
+        Float e = 1.12345f;
+        System.out.println(e);
+        Character f = 'あ';
+        System.out.println(f);
+        
+        int[] array = { 1, 2, 3, 4, 5 };
+        System.out.println(array[3]);
+    }
+}
