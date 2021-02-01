6cde4fcbb2951511508e67624446832f9f4031c1
野崎梨緒 <wada.rio1001@gmail.com>
2021-02-01 21:28:51 +0900

    Finish lesson_10

---
+++ JavaWcp_basic/src/exception/Exception02.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class Exception02 {
+	public static void main(String[] agrs) {
+		try {
+			System.out.println("100 ÷ 0 は？");
+			int result = 100 / 0;
+			System.out.println("試算結果" + result);
+		} catch (ArithmeticException e) {
+			System.out.println("例外が発生");
+		} finally {
+			System.out.println("プログラム終了");
+		}
+	}
+}
